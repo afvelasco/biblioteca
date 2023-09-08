3cf3224a65b9d218b6a3de9c1080446d82c08d3b
Edinson Sarria <sarria1989@hotmail.com>
2023-09-08 11:16:57 -0500

    Actualizacion Ruta

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/')
+def actualizar():("actualizar.html")
+
 if __name__=='__main__':
     app.run(host="0.0.0.0", debug=True,port="8000")
