25676858b1fe645aae4fc42ecc84374f2a219bc7
santvg98 <139291797+santvg98@users.noreply.github.com>
2023-09-08 16:23:28 -0500

    Esto es una prueba

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def hello():
+    return "Hola, si funciona desde casa "
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
 @app.route('/')
 def actualizar():("actualizar.html")
+
+
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", debug=True,port="8000")
